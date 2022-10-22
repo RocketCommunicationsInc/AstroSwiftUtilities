3ce7393cea5dd786a37da447e5c963596c4140d6
Jeff Hokit <jhokit@rocketcom.com>
2022-10-21 17:02:08 -0700

    build: rename to AstroSwiftUtilities

--- Tests/AstroSwiftUtilitiesTests/AstroSwiftUtilitiesTests.swift
+++ Tests/AstroSwiftUtilitiesTests/AstroSwiftUtilitiesTests.swift
@@ -1,11 +1,11 @@
 import XCTest
-@testable import SwiftUtilities
+@testable import AstroSwiftUtilities
 
-final class SwiftUtilitiesTests: XCTestCase {
+final class AstroSwiftUtilitiesTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(SwiftUtilities().text, "Hello, World!")
+        XCTAssertEqual(AstroSwiftUtilities().text, "Hello, World!")
     }
 }
