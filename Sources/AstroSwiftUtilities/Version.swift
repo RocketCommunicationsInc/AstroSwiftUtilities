081ae0b11a7ddbd2c232da975e009783af3a7e0d
Jeff Hokit <jhokit@rocketcom.com>
2022-11-10 13:07:56 -0800

    feat: Add versionString function

---
+++ Sources/AstroSwiftUtilities/Version.swift
@@ -0,0 +1,14 @@
+//
+//  Version.swift
+//
+//  Created by rocketjeff on 7/1/22.
+//
+
+import Foundation
+
+func versionString()->String
+{
+    let version:String = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? "Unknown"
+    let build = Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as? String ?? "Unknown"
+    return "Version " + version + " (" + build + ")"
+}
